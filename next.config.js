d199dada78d6fddf48f33ca78e5eb9ccab3982f2
Sarvesh Mhadgut <sarveshmhadgut@icloud.com>
2025-08-05 18:55:53 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['links.papareact.com', 'cloud.appwrite.io'],
-    },
-}
-
-module.exports = nextConfig
